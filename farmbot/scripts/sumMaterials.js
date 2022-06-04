fbafc3531efe6574d7ea67da75a975577fbc4be1
george <g.george.jone@gmail.com>
2022-06-04 14:07:16 -0700

    change td to TD

--- farmbot/scripts/sumMaterials.js
+++ farmbot/scripts/sumMaterials.js
@@ -6,8 +6,7 @@
   console.log({rows});
 
   for (const tr of rows) {
-    console.log({tr, children: tr.children, tagName: tr.tagName})
-    if (!tr.children.length || tr.children[0].tagName !== 'td') {
+    if (!tr.hasChildNodes || tr.children[0].tagName !== 'TD') {
       continue;
     }
     console.log(tr.children);
