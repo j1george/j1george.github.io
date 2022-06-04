d0f34b6bab63c848a417576b6b7589d7ae80b810
George Jone <g.george.jone@gmail.com>
2022-06-04 16:50:16 -0700

    rename materials checklist file

---
+++ farmbot/scripts/checklistMaterials.js
@@ -0,0 +1,17 @@
+(() => {
+  const materialsTable = document.getElementById('materials');
+
+  const rows = [...materialsTable.getElementsByTagName('tr')];
+
+  for (const tr of rows.slice(1, rows.length - 1)) {
+
+    const children = tr.children
+
+    if (!children.length) {
+      continue;
+    }
+
+    //$(tr).prepend('<input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">')
+  }
+
+})();
