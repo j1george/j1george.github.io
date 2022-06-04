787da65260c97b9220bb5097578b4688eabe76b3
george <g.george.jone@gmail.com>
2022-06-04 14:13:37 -0700

    print child innerhtml

--- farmbot/scripts/sumMaterials.js
+++ farmbot/scripts/sumMaterials.js
@@ -1,15 +1,16 @@
 (() => {
   const materialsTable = document.getElementById('materials');
 
-  const rows = materialsTable.getElementsByTagName('tr');
+  const rows = [...materialsTable.getElementsByTagName('tr')];
 
   console.log({rows});
 
-  for (const tr of rows) {
-    if (!tr.hasChildNodes || tr.children[0].tagName !== 'TD') {
+  let sum = 0;
+  for (const tr of rows.slice(1, rows.length - 1)) {
+    if (!tr.children.length) {
       continue;
     }
-    console.log(tr.children);
+    console.log(tr.children[0].innerHTML);
   }
 
 })();
