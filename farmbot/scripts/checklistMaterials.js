9fb37fe020292bac5bb763dc3c99d2db6c481c9e
George Jone <g.george.jone@gmail.com>
2022-06-04 17:28:55 -0700

    add checkboxes

--- farmbot/scripts/checklistMaterials.js
+++ farmbot/scripts/checklistMaterials.js
@@ -3,7 +3,8 @@
 
   const rows = [...materialsTable.getElementsByTagName('tr')];
   const trHead = rows[0];
-  $(trHead).prepend('<th>Done</th>')
+  $(trHead).prepend('<th>Done</th>');
+  
   for (const i in rows.slice(1, rows.length - 1)) {
     console.log(i);
     const tr = rows[i + 1];
@@ -15,6 +16,7 @@
 
     $(tr).prepend(`<input type="checkbox" id="chk${i}" name="chk${i}" value="${children[1].innerHTML}">`)
   }
+
   const trTotal = rows[rows.length - 1];
   $(trTotal).prepend('<td></td>');
 
