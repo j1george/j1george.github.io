53eb5570ef1fdad184b09cd4a6c9e856e2a187d8
George Jone <g.george.jone@gmail.com>
2022-06-05 18:25:54 -0700

    remove iife

--- farmbot/scripts/checklistSteps.js
+++ farmbot/scripts/checklistSteps.js
@@ -1,67 +1,64 @@
-await (async () => {
 
-  const hit = async (id) => {
-    const res = await fetch(`https://api.countapi.xyz/hit/gj.farmbot.steps/${id}`, {
-      method: 'GET',
-      redirect: 'follow'
-    })
-    .then(res => res.json());
+const hit = async (id) => {
+  const res = await fetch(`https://api.countapi.xyz/hit/gj.farmbot.steps/${id}`, {
+    method: 'GET',
+    redirect: 'follow'
+  })
+  .then(res => res.json());
 
-    return res;
-  }
+  return res;
+}
 
-  const isChecked = async (id) => {
-    const res = await fetch(`https://api.countapi.xyz/get/gj.farmbot.steps/${id}`, {
-      method: 'GET',
-      redirect: 'follow'
-    })
-    .then(async res => {
+const isChecked = async (id) => {
+  const res = await fetch(`https://api.countapi.xyz/get/gj.farmbot.steps/${id}`, {
+    method: 'GET',
+    redirect: 'follow'
+  })
+  .then(async res => {
 
-      if (res.ok) {
-        return res.json();
-      }
-      
-      return await hit(id);
-    })
-    .then(data => data.value);
-    
-    count = res ? res : 0;
-
-    return count % 2 === 0;
-  };
-
-  const stepsDiv = document.getElementById('accordion');
-
-  const stepHeaders = [...stepsDiv.getElementsByTagName('h1')];
-
-  for (const i in stepHeaders) {
-    const stepHeader = $(stepHeaders[parseInt(i)]);
-
-    const id = `step${i}`
-
-    stepHeader.attr('id', id);
-
-    stepHeader.attr('class', await isChecked(id) ? 'show' : 'hide');
-
-    const isHidden = stepHeader.hasClass('hide');
-
-    if (isHidden) {
-      stepHeader.click(function() {
-        $(this).next().toggle('slow');
-    
-        stepHeader.attr('class', isHidden ? 'show' : 'hide');
-        hit(id);
-        return true;
-      }).next().hide();
-    } else {
-      stepHeader.click(function() {
-        $(this).next().toggle('slow');
-    
-        stepHeader.attr('class', isHidden ? 'show' : 'hide');
-        hit(id);
-        return true;
-      }).next().show();
+    if (res.ok) {
+      return res.json();
     }
-  }
+    
+    return await hit(id);
+  })
+  .then(data => data.value);
+  
+  count = res ? res : 0;
 
-})();
+  return count % 2 === 0;
+};
+
+const stepsDiv = document.getElementById('accordion');
+
+const stepHeaders = [...stepsDiv.getElementsByTagName('h1')];
+
+for (const i in stepHeaders) {
+  const stepHeader = $(stepHeaders[parseInt(i)]);
+
+  const id = `step${i}`
+
+  stepHeader.attr('id', id);
+
+  stepHeader.attr('class', await isChecked(id) ? 'show' : 'hide');
+
+  const isHidden = stepHeader.hasClass('hide');
+
+  if (isHidden) {
+    stepHeader.click(function() {
+      $(this).next().toggle('slow');
+  
+      stepHeader.attr('class', isHidden ? 'show' : 'hide');
+      hit(id);
+      return true;
+    }).next().hide();
+  } else {
+    stepHeader.click(function() {
+      $(this).next().toggle('slow');
+  
+      stepHeader.attr('class', isHidden ? 'show' : 'hide');
+      hit(id);
+      return true;
+    }).next().show();
+  }
+}
