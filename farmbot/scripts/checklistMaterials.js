c0b8e863787708ab3ac848e13cd5dc095b913e4d
George Jone <g.george.jone@gmail.com>
2022-06-05 18:16:24 -0700

    remove iife

--- farmbot/scripts/checklistMaterials.js
+++ farmbot/scripts/checklistMaterials.js
@@ -1,59 +1,54 @@
-const fnc = async () => {
 
-  const hit = async (id) => {
-    const res = await fetch(`https://api.countapi.xyz/hit/gj.farmbot.materials/${id}`, {
-      method: 'GET',
-      redirect: 'follow'
-    })
-    .then(res => res.json());
+const hit = async (id) => {
+  const res = await fetch(`https://api.countapi.xyz/hit/gj.farmbot.materials/${id}`, {
+    method: 'GET',
+    redirect: 'follow'
+  })
+  .then(res => res.json());
 
-    return res;
-  }
+  return res;
+}
 
-  const isChecked = async (id) => {
-    const res = await fetch(`https://api.countapi.xyz/get/gj.farmbot.materials/${id}`, {
-      method: 'GET',
-      redirect: 'follow'
-    })
-    .then(async res => {
+const isChecked = async (id) => {
+  const res = await fetch(`https://api.countapi.xyz/get/gj.farmbot.materials/${id}`, {
+    method: 'GET',
+    redirect: 'follow'
+  })
+  .then(async res => {
 
-      if (res.ok) {
-        return res.json();
-      }
-      
-      return await hit(id);
-    })
-    .then(data => data.value);
-    
-    count = res ? res : 0;
-
-    return count % 2 === 0;
-  };
-
-  const materialsTable = document.getElementById('materials');
-
-  const rows = [...materialsTable.getElementsByTagName('tr')];
-  const trHead = rows[0];
-  $(trHead).prepend('<th>Acquired</th>');
-
-  for (const i in rows.slice(1, rows.length - 1)) {
-    const tr = rows[parseInt(i) + 1];
-    const children = tr.children
-
-    if (!children.length) {
-      continue;
+    if (res.ok) {
+      return res.json();
     }
+    
+    return await hit(id);
+  })
+  .then(data => data.value);
+  
+  count = res ? res : 0;
 
-    const id = `mat${i}`
-    $(tr).prepend(`<td><input type="checkbox" id="${id}" name="${id}" value="${children[1].innerHTML}" ${await isChecked(id) ? 'checked' : ''}></td>`);
-    $(`#${id}`).click(function() {
-      hit(id);
-    });
-  }
-
-  const trTotal = rows[rows.length - 1];
-  $(trTotal).prepend('<td></td>');
-
+  return count % 2 === 0;
 };
 
-await fnc();
+const materialsTable = document.getElementById('materials');
+
+const rows = [...materialsTable.getElementsByTagName('tr')];
+const trHead = rows[0];
+$(trHead).prepend('<th>Acquired</th>');
+
+for (const i in rows.slice(1, rows.length - 1)) {
+  const tr = rows[parseInt(i) + 1];
+  const children = tr.children
+
+  if (!children.length) {
+    continue;
+  }
+
+  const id = `mat${i}`
+  $(tr).prepend(`<td><input type="checkbox" id="${id}" name="${id}" value="${children[1].innerHTML}" ${await isChecked(id) ? 'checked' : ''}></td>`);
+  $(`#${id}`).click(function() {
+    hit(id);
+  });
+}
+
+const trTotal = rows[rows.length - 1];
+$(trTotal).prepend('<td></td>');
