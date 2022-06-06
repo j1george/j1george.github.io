e5944210f754784ae222646b1dc811c320684c75
George Jone <g.george.jone@gmail.com>
2022-06-05 18:46:53 -0700

    await iife

--- farmbot/scripts/checklistMaterials.js
+++ farmbot/scripts/checklistMaterials.js
@@ -1,54 +1,59 @@
+const fnc = async () => {
 
-const hitMats = async (id) => {
-  const res = await fetch(`https://api.countapi.xyz/hit/gj.farmbot.materials/${id}`, {
-    method: 'GET',
-    redirect: 'follow'
-  })
-  .then(res => res.json());
+  const hit = async (id) => {
+    const res = await fetch(`https://api.countapi.xyz/hit/gj.farmbot.materials/${id}`, {
+      method: 'GET',
+      redirect: 'follow'
+    })
+    .then(res => res.json());
 
-  return res;
-}
-
-const isCheckedMats = async (id) => {
-  const res = await fetch(`https://api.countapi.xyz/get/gj.farmbot.materials/${id}`, {
-    method: 'GET',
-    redirect: 'follow'
-  })
-  .then(async res => {
-
-    if (res.ok) {
-      return res.json();
-    }
-    
-    return await hitMats(id);
-  })
-  .then(data => data.value);
-  
-  count = res ? res : 0;
-
-  return count % 2 === 0;
-};
-
-const materialsTable = document.getElementById('materials');
-
-const rowsMats = [...materialsTable.getElementsByTagName('tr')];
-const trHead = rowsMats[0];
-$(trHead).prepend('<th>Acquired</th>');
-
-for (const i in rowsMats.slice(1, rowsMats.length - 1)) {
-  const tr = rowsMats[parseInt(i) + 1];
-  const children = tr.children
-
-  if (!children.length) {
-    continue;
+    return res;
   }
 
-  const id = `mat${i}`
-  $(tr).prepend(`<td><input type="checkbox" id="${id}" name="${id}" value="${children[1].innerHTML}" ${isCheckedMats(id) ? 'checked' : ''}></td>`);
-  $(`#${id}`).click(function() {
-    hitMats(id);
-  });
-}
+  const isChecked = async (id) => {
+    const res = await fetch(`https://api.countapi.xyz/get/gj.farmbot.materials/${id}`, {
+      method: 'GET',
+      redirect: 'follow'
+    })
+    .then(async res => {
 
-const trTotal = rowsMats[rowsMats.length - 1];
-$(trTotal).prepend('<td></td>');
+      if (res.ok) {
+        return res.json();
+      }
+      
+      return await hit(id);
+    })
+    .then(data => data.value);
+    
+    count = res ? res : 0;
+
+    return count % 2 === 0;
+  };
+
+  const materialsTable = document.getElementById('materials');
+
+  const rows = [...materialsTable.getElementsByTagName('tr')];
+  const trHead = rows[0];
+  $(trHead).prepend('<th>Acquired</th>');
+
+  for (const i in rows.slice(1, rows.length - 1)) {
+    const tr = rows[parseInt(i) + 1];
+    const children = tr.children
+
+    if (!children.length) {
+      continue;
+    }
+
+    const id = `mat${i}`
+    $(tr).prepend(`<td><input type="checkbox" id="${id}" name="${id}" value="${children[1].innerHTML}" ${await isChecked(id) ? 'checked' : ''}></td>`);
+    $(`#${id}`).click(function() {
+      hit(id);
+    });
+  }
+
+  const trTotal = rows[rows.length - 1];
+  $(trTotal).prepend('<td></td>');
+
+};
+
+fnc();
--- farmbot/scripts/checklistSteps.js
+++ farmbot/scripts/checklistSteps.js
@@ -1,64 +1,67 @@
+(async function() {
 
-const hitSteps = async (id) => {
-  const res = await fetch(`https://api.countapi.xyz/hit/gj.farmbot.steps/${id}`, {
-    method: 'GET',
-    redirect: 'follow'
-  })
-  .then(res => res.json());
+  const hit = async (id) => {
+    const res = await fetch(`https://api.countapi.xyz/hit/gj.farmbot.steps/${id}`, {
+      method: 'GET',
+      redirect: 'follow'
+    })
+    .then(res => res.json());
 
-  return res;
-}
-
-const isCheckedSteps = async (id) => {
-  const res = await fetch(`https://api.countapi.xyz/get/gj.farmbot.steps/${id}`, {
-    method: 'GET',
-    redirect: 'follow'
-  })
-  .then(async res => {
-
-    if (res.ok) {
-      return res.json();
-    }
-    
-    return await hitMats(id);
-  })
-  .then(data => data.value);
-  
-  count = res ? res : 0;
-
-  return count % 2 === 0;
-};
-
-const stepsDiv = document.getElementById('accordion');
-
-const stepHeaders = [...stepsDiv.getElementsByTagName('h1')];
-
-for (const i in stepHeaders) {
-  const stepHeader = $(stepHeaders[parseInt(i)]);
-
-  const id = `step${i}`
-
-  stepHeader.attr('id', id);
-
-  stepHeader.attr('class', isCheckedMats(id) ? 'show' : 'hide');
-
-  const isHidden = stepHeader.hasClass('hide');
-
-  if (isHidden) {
-    stepHeader.click(function() {
-      $(this).next().toggle('slow');
-  
-      stepHeader.attr('class', isHidden ? 'show' : 'hide');
-      hitMats(id);
-      return true;
-    }).next().hide();
-  } else {
-    stepHeader.click(function() {
-      $(this).next().toggle('slow');
-  
-      stepHeader.attr('class', isHidden ? 'show' : 'hide');
-      hitMats(id);
-      return true;
-    }).next().show();
+    return res;
   }
-}
+
+  const isChecked = async (id) => {
+    const res = await fetch(`https://api.countapi.xyz/get/gj.farmbot.steps/${id}`, {
+      method: 'GET',
+      redirect: 'follow'
+    })
+    .then(async res => {
+
+      if (res.ok) {
+        return res.json();
+      }
+      
+      return await hit(id);
+    })
+    .then(data => data.value);
+    
+    count = res ? res : 0;
+
+    return count % 2 === 0;
+  };
+
+  const stepsDiv = document.getElementById('accordion');
+
+  const stepHeaders = [...stepsDiv.getElementsByTagName('h1')];
+
+  for (const i in stepHeaders) {
+    const stepHeader = $(stepHeaders[parseInt(i)]);
+
+    const id = `step${i}`
+
+    stepHeader.attr('id', id);
+
+    stepHeader.attr('class', await isChecked(id) ? 'show' : 'hide');
+
+    const isHidden = stepHeader.hasClass('hide');
+
+    if (isHidden) {
+      stepHeader.click(function() {
+        $(this).next().toggle('slow');
+    
+        stepHeader.attr('class', isHidden ? 'show' : 'hide');
+        hit(id);
+        return true;
+      }).next().hide();
+    } else {
+      stepHeader.click(function() {
+        $(this).next().toggle('slow');
+    
+        stepHeader.attr('class', isHidden ? 'show' : 'hide');
+        hit(id);
+        return true;
+      }).next().show();
+    }
+  }
+
+})();
